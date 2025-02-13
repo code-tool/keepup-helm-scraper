8a0feca354512497cec69862e3c5ade0b72dac8b
foxhound <foxhound@foxhound.sot.com>
2025-02-13 20:16:05 +0200

    rm extra files

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-kubectl apply -f kubernetes/sa.yaml
-kubectl apply -f kubernetes/role.yaml
-kubectl apply -f kubernetes/binding.yaml
-kubectl apply -f kubernetes/clusterrole.yaml
-kubectl apply -f kubernetes/clusterrolebinding.yaml
-kubectl apply -f kubernetes/cronjob.yaml
